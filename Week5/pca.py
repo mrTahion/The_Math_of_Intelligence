a9636928087aede5b399adf68e9c663353b3768c
mrTahion <naulalica@gmail.com>
2018-09-25 23:25:08 +0700

    week5 tiny fix

--- Week5/pca.py
+++ Week5/pca.py
@@ -14,16 +14,16 @@ columns_drop= ['Type']
 y_train = data.Type
 X_train = data.drop(columns_drop, axis=1)
 
-# data prepdor PCA
-PCAdata = (X_train - np.mean(X_train, axis=0)) / np.std(X_train, axis=0)
+# data prep for PCA
+PCA_data = (X_train - np.mean(X_train, axis=0)) / np.std(X_train, axis=0)
 
 # Getting covariance matrix
-cov = np.matmul(PCAdata.transpose(), PCAdata) / (PCAdata.shape[0] - 1)
+cov = np.matmul(PCA_data.transpose(), PCA_data) / (PCA_data.shape[0] - 1)
 # Get eigen values and axis
 eig, axes = np.linalg.eig(cov)
 
 # calculete components for 2d - 2 largest eigen values
-pc2d = np.matmul(PCAdata, axes[:, 0:2])
+pc2d = np.matmul(PCA_data, axes[:, 0:2])
 pc1 = np.array(pc2d[:, 0].transpose())
 pc2 = np.array(pc2d[:, 1].transpose())
 
